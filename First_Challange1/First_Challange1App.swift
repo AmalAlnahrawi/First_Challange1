1947bb2f8bc8110c1ca890a34dbd1b3d332f2aa0
linanawaf <linanawafalharbi@gmail.com>
2024-09-26 17:05:59 +0300

    Initial Commit

---
+++ First_Challange1/First_Challange1App.swift
@@ -0,0 +1,17 @@
+//
+//  First_Challange1App.swift
+//  First_Challange1
+//
+//  Created by Lina Alharbi on 23/03/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct First_Challange1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
